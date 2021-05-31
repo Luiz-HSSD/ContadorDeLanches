7ae2585d40a7b03f73b7645d79d4cf79879e52a3
Luiz Henrique Santos Sousa Diniz <lhenrique_diniz@hotmail.com>
2021-05-30 23:25:09 -0300

    Adicionar arquivos de projeto.

---
+++ DBInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContadorDeLanches
+{
+    public class DBInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<LancheContexto>
+    {
+        protected override void Seed(LancheContexto context)
+        {
+            var lanches = new List<Lanche>
+            {
+                new Lanche(){Id=1, Nome="x-burger" },
+                new Lanche(){Id=2, Nome="x-salada" },
+                new Lanche(){Id=3, Nome="x-bacon" },
+                new Lanche(){Id=4, Nome="x-frango" },
+                new Lanche(){Id=5, Nome="x-tudo" },
+                new Lanche(){Id=6, Nome="brutos" },
+                new Lanche(){Id=7, Nome="x-calabreza" },
+                new Lanche(){Id=8, Nome="x-egg" },
+                new Lanche(){Id=9, Nome="x-burger" },
+                new Lanche(){Id=10, Nome="x-burger" },
+            };
+            lanches.ForEach(s => context.Lanches.Add(s));
+            context.SaveChanges();
+            //context.SaveChanges();
+        }
+    }
+}
---
+++ LancheDia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContadorDeLanches
+{
+    public class LancheDia
+    {
+        [Key]
+        public long Id { get; set; }
+        //[ForeignKey("Lanche")]
+        public long IdLanche { get; set; }
+        public long Qtd { get; set; }
+        public DateTime Dia { get; set; }
+    }
+}
